f15e570c91ebfc9058c291fff88f1721f4c78383
Simon Duff <simon.j.duff@gmail.com>
2019-12-06 11:35:47 +0000

    Make keys strings not dicts

--- room.py
+++ room.py
@@ -2,14 +2,14 @@
 
 def loadRooms():
     rooms = {"1": {"commands":{
-                    {"arrive": {"type": "text",
+                    "arrive": {"type": "text",
                                 "text": "You slowly open your eyes, despite a throbbing headache",
                                 "valid": "all"}
-                                },
-                    {"east": {"type": "exit",
+                                ,
+                    "east": {"type": "exit",
                             "room": 2,
                             "valid" : "all"}
-                        }
+                    }
                 }
     }
     }
